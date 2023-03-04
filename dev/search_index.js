9d2375832578130506a365176b6f600cc75b2dd2
Documenter.jl <documenter@juliadocs.github.io>
2023-03-04 04:55:15 +0000

    build based on 5601976

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DataNetworkModels.jl","page":"DataNetworkModels.jl","title":"DataNetworkModels.jl","text":"","category":"section"},{"location":"","page":"DataNetworkModels.jl","title":"DataNetworkModels.jl","text":"Documentation for DataNetworkModels.jl","category":"page"}]
+}
